7869820dede378b139284f606bf270519d438bb9
igor <perrofeofilms@gmail.com>
2019-12-06 12:22:56 -0600

    -

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,6 +51,7 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Motxuelo<i>lab</i></a></li>
+		<li><a class='{segment === "portfolio" ? "selected" : ""}' href='portfolio'>portfolio</a></li>
 		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
